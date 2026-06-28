90e3536e1d210f8d352af69c273b35d75c0a19a9
ABHAY <121710071+Villionare@users.noreply.github.com>
2026-06-28 15:05:02 +0530

    Initial Commit

---
+++ src/main/java/com/abhay/diddy/DiddyApplication.java
@@ -0,0 +1,13 @@
+package com.abhay.diddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhay/diddy/DiddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhay.diddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
